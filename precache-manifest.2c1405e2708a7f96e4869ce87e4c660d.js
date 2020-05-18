70abc83bd9cc8116c80de28524b3ece6eb09df01
timwf <timwfowler0@gmail.com>
2020-05-18 10:35:08 +0700

    Updates

---
+++ precache-manifest.2c1405e2708a7f96e4869ce87e4c660d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b921c893d75855b0ce7dbdbf5950564",
+    "url": "/portfolio-demo/index.html"
+  },
+  {
+    "revision": "ec7475273e4bb1440c63",
+    "url": "/portfolio-demo/static/css/main.168d7cbf.chunk.css"
+  },
+  {
+    "revision": "3c8c5d9cede9e269b4fd",
+    "url": "/portfolio-demo/static/js/2.147ce4e4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio-demo/static/js/2.147ce4e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec7475273e4bb1440c63",
+    "url": "/portfolio-demo/static/js/main.09dc2b75.chunk.js"
+  },
+  {
+    "revision": "368fc81553478f3edd83",
+    "url": "/portfolio-demo/static/js/runtime-main.7d6d2790.js"
+  },
+  {
+    "revision": "2c0c9199897425159821731da560129c",
+    "url": "/portfolio-demo/static/media/brain-map.2c0c9199.png"
+  },
+  {
+    "revision": "cec09a757d3f3730e79f8080d7fb707b",
+    "url": "/portfolio-demo/static/media/profile-pic.cec09a75.png"
+  }
+]);
